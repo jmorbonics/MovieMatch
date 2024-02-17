a4e9adef84044a65f76fb782ebd3e4ded7aff04a
CooperRoalson <csroalson@gmail.com>
2024-02-17 04:48:02 -0800

    Replaced demo source code

--- App.jsx
+++ App.jsx
@@ -1,63 +1,10 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { CONVEX_URL } from "@env";
-import React, { StrictMode, useState } from "react";
-import { FlatList, SafeAreaView, Text, TextInput, View } from "react-native";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "./convex/_generated/api";
-import styles from "./styles";
+import React, { StrictMode } from "react";
+import MusicMatchApp from "./src/MusicMatch";
 
-function InnerApp() {
-  const messages = useQuery(api.messages.list) || [];
 
-  const [newMessageText, setNewMessageText] = useState("");
-  const sendMessage = useMutation(api.messages.send);
-
-  const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
-  async function handleSendMessage(event) {
-    event.preventDefault();
-    setNewMessageText("");
-    await sendMessage({ body: newMessageText, author: name });
-  }
-
-  return (
-    <SafeAreaView style={styles.body}>
-      <Text style={styles.title}>Hi there</Text>
-      <View style={styles.name}>
-        <Text style={styles.nameText} testID="NameField">
-          {name}
-        </Text>
-      </View>
-      <FlatList
-        data={messages.slice(-10)}
-        testID="MessagesList"
-        renderItem={(x) => {
-          const message = x.item;
-          return (
-            <View style={styles.messageContainer}>
-              <Text>
-                <Text style={styles.messageAuthor}>{message.author}:</Text>{" "}
-                {message.body}
-              </Text>
-              <Text style={styles.timestamp}>
-                {new Date(message._creationTime).toLocaleTimeString()}
-              </Text>
-            </View>
-          );
-        }}
-      />
-      <TextInput
-        placeholder="Write a message…"
-        style={styles.input}
-        onSubmitEditing={handleSendMessage}
-        onChangeText={(newText) => setNewMessageText(newText)}
-        defaultValue={newMessageText}
-        testID="MessageInput"
-      />
-    </SafeAreaView>
-  );
-}
-
-const App = () => {
+export default App = () => {
   const convex = new ConvexReactClient(CONVEX_URL, {
     // We need to disable this to be compatible with React Native
     unsavedChangesWarning: false,
@@ -65,10 +12,8 @@ const App = () => {
   return (
     <StrictMode>
       <ConvexProvider client={convex}>
-        <InnerApp />
+        <MusicMatchApp />
       </ConvexProvider>
     </StrictMode>
   );
 };
-
-export default App;
