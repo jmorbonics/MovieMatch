ca45d2c1e9ea0a20210b39ebe636b5ec26ae632e
Jack Morby <jmorby2@illinois.edu>
2024-02-18 03:36:10 -0800

    changing style

--- screens/feed_screen.jsx
+++ screens/feed_screen.jsx
@@ -2,14 +2,11 @@ import React, { useState, useEffect, useContext } from "react";
 import { Text, SafeAreaView, Button } from "react-native";
 import styles from "../styles";
 import { UserContext } from "../App";
+import MovieRecs from "../components/movierecs.component";
 
 export default FeedScreen = ({ navigation, route }) => {
     const { username, userData, setUserData } = useContext(UserContext);
-    
-    let val = userData.testVal;
-    if (val === undefined) {
-        val = 0;
-    }
+    val = "012"
 
     function handleIncrement() {
         setUserData({ username: username, data: {testVal: val + 1 }});
@@ -18,13 +15,15 @@ export default FeedScreen = ({ navigation, route }) => {
     return (
         <SafeAreaView style={styles.body}>
             <>
-                <Text style={styles.title}>Hello {username}!</Text>
-                <Text style={styles.title}>Your var is {val}!</Text>
+                <Text style={styles.title}>Hello {username}
+                Here is your feed!</Text>
+                {/* <Text style={styles.title}>Your var is {val}!</Text>
                 <Button
                     onPress={handleIncrement}
                     title="Increment!"
                     style={styles.button}
-                />
+                /> */}
+                <MovieRecs searchWord={val}></MovieRecs>
             </>
         </SafeAreaView>
     );
